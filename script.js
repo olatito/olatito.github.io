4b397b984867d8bbb83e877767868d9850f5f4b7
TITO's MACBOOK <user@TITOs-MacBook-Pro.local>
2023-09-09 10:04:16 +0100

    main

--- script.js
+++ script.js
@@ -28,22 +28,3 @@ const currentTime = `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
 
 dayOfTheWeek.textContent = currentDay;
 timeOfTheDay.textContent = currentTime;
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Get the current day of the week
-//   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//   const currentDate = new Date();
-//   const currentDay = daysOfWeek[currentDate.getUTCDay()];
-//   const currentUTCTime = currentDate.getTime();
-
-//   // Convert milliseconds to a Date object
-//   const currentUTCTimeDate = new Date(currentUTCTime);
-
-//   // Get hours and minutes in 24-hour format
-//   const hours = currentUTCTimeDate.getUTCHours();
-//   const minutes = currentUTCTimeDate.getUTCMinutes();
-
-//   // Update the elements with dynamic data
-//   document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
-//   document.querySelector('[data-testid="currentUTCTime"]').textContent = `${hours}:${(minutes < 10 ? '0' : '')}${minutes}`;
-// });
